1923bb3088d060bcc398a63fa1041c39a7b6cf31
izharabbasi <izharabbasi103@gmail.com>
2020-09-21 01:44:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,173 @@
+//alert("JavaScript Alert \n\n\n  Welcome to jsLand... \n Happy Coding!..." )
+
+// var name = "Izhar Tanveer";
+// var age = "15 years";
+
+// var message = "hello world this is izhar from pakistan"
+
+
+// alert(name);
+// alert(age);
+// alert(message);
+
+// var email = "izharabbaso103"
+
+// alert(email + "@gmail.com")
+
+//document.write("Yes i can write content through JS")
+
+// var birth_year = "15 year"
+
+// document.write("My birth Year is " + birth_year)
+
+// var num1 = 3;
+// var num2 = 4;
+
+// var sum = num1 + num2;
+
+// document.write("the sum of " + num1 + " and " + num2 + " is " + sum)
+
+// var n;
+
+// document.write("The value of name before is " + n + "<br>");
+// n = 2;
+// document.write("Intial Value: " + n + "<br>")
+// n++;
+// document.write("value After increment: " + n + "<br>")
+// n= n + 7;
+// document.write("Value after addition: " + n + "<br>")
+// n--;
+// document.write("Value after decrement: " + n + "<br>")
+// n % 3;
+// document.write("value After division: " + n + "<br>")
+
+// var dollar = 10 * 165;
+// var riyal = 25 * 41;
+
+// var pkr = dollar + riyal;
+// document.write("<h1> Currency in PKR </h1>")
+// document.write("<b>Total currency is: </b>" +pkr )
+
+// var city = prompt("Enter city Name: ");
+
+// if (city=="Karachi"){
+//     alert("Wlecome to city of lights!");
+// }
+
+// var signal = prompt("Enter Traffic Light color ");
+
+// if (signal=="red"){
+//     alert("Must stop");
+// }
+// else if (signal=="yellow"){
+//     alert("Ready to move");
+// }
+// else if (signal=="green"){
+//     alert("Move now")
+// }
+
+// var secret = 10;
+// var user_input = prompt("Enter Secret Number");
+
+// if (secret==user_input){
+//     alert("You are correct");
+// }
+// else if (secret== user_input || +1){
+//     alert("Too close");
+// }
+// else{
+//     alert("Try Again!")
+// }
+
+// var even = prompt("Enter Number:");
+
+// if (even%2==0){
+//     alert("you type even number");
+// }
+// else if(even%2==1){
+//     alert("You typed odd number");
+// }
+
+// var first_number = Number(prompt("Enter number"));
+// var second_number = Number(prompt("Enter number"));
+// var calculation = prompt("Apply calcultaion");
+
+// if (calculation=="+"){
+//     alert(first_number + second_number)
+// }
+// else if(calculation=="-"){
+//     alert(first_number - second_number)
+// }
+// else if (calculation=="*"){
+//     alert(first_number * second_number)
+// }
+// else if (calculation=="/"){
+//     alert(first_number / second_number)
+// }
+// else{
+//     alert("Please Enter Number")
+// }
+
+// var number = prompt("Enter Number: ");
+
+// if (number>0){
+//     alert("your number is positive")
+// }
+// else if (number <0){
+//     alert("your number is negative")
+// }
+// else if (number== 0){
+//     alert("your number is zero")
+// }
+
+// var vowel = prompt("Enter vowel")
+
+// if (vowel == "a" || "e" || "i" || "o" || "u"){
+//     alert("you enter vowel");
+// }
+// else{
+//     alert("You haven't typed vowel");
+// }
+
+// var qualification = ['SSC',"HSC", "BCS"];
+
+// console.log("Qualification", "\n");
+
+// console.log(qualification[0]);
+// console.log(qualification[1]);
+// console.log(qualification[2]);
+
+// var students = ["Izhar", "Shery", "Ahmed"];
+// var marks = [320, 400, 340];
+// var total_marks = 500;
+
+// console.log("Score of " + students[0] + " is " +marks[0] + " percentage  is " + total_marks*100/marks[0]);
+// console.log("Score of " + students[1] + " is " +marks[1] + " percentage  is " + total_marks*100/marks[1]);
+// console.log("Score of " + students[2] + " is " +marks[2] + " percentage  is " + total_marks*100/marks[2]);
+
+// var cities = ["khi","lhr","isb","rwl","mul"];
+
+// console.log(cities);
+
+// var s_cities = [cities[2],cities[1],cities[3]];
+// console.log(s_cities);
+
+var color =  ['red', 'blue', 'green', 'black', 'pink ' , "</br>"];
+document.write(color);
+
+color.unshift(prompt("Please enter color that you want to add in beggining"));
+document.write(color);
+
+color.unshift(prompt("Please enter color that you want to add in beggining"));
+document.write(color);
+
+color.unshift(prompt("Please enter color that you want to add in beggining"));
+document.write(color);
+
+color.shift();
+
+document.write(color)
+
+color.pop();
+
+document.write(color)
